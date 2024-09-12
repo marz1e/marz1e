5b4e4220ff4954b0cbce1654b6bb0ac9ba5aacdf
marzie <notmarzie@gmail.com>
2024-09-12 18:26:55 +1200

    this is my first coding project thingie
    it is very simple and bad. hopefully i wil get better.

---
+++ app.py
@@ -0,0 +1,9 @@
+
+print("Hello, welcome to Cool Guy Coffee Shop.")
+name = input("What is your name?")
+print("Hello," + name, "What would you like to order?")
+print("One coffee coming up.")
+print("Thank you for coming.")
+#i am adding comments
+
+
