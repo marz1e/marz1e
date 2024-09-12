8498805730170374c2abf5a04ebaa745221eb12f
marzie <notmarzie@gmail.com>
2024-09-12 18:28:23 +1200

    question

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ name = input("What is your name?")
 print("Hello," + name, "What would you like to order?")
 print("One coffee coming up.")
 print("Thank you for coming.")
-#i am adding comments
+#i am adding comments.
+#how do i add this to my coding journey repository instead of making it a different thing?
 
 
